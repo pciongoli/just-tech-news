59e44d0d4ef13b56a22e670c8294063f7fd83d4e
pcion <patrick.ciongoli@gmail.com>
2022-03-22 18:22:38 -0400

    created the User model

---
+++ models/User.js
@@ -0,0 +1,67 @@
+// import Model and DataTypes object from Sequelize
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// init method to initialize all of the model's data and configuration
+// define table columns and configuration
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+        // define an id column
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is 
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the primary key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        // define a user column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false, we can run oru data through validators before creating the table data
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least 4 characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // dont't automatically create createAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+        // make it so our model name stays lowercase in the databse
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
