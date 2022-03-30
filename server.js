9cd3ec2aef196a1b955af04f3d46e462c2774c2d
pcion <patrick.ciongoli@gmail.com>
2022-03-30 16:27:31 -0400

    set up project

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -7,16 +8,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
-
-
 // if the ({ force false }) were to be set to true it would drop and re-create all of the database tables on startup
-// good for when 
+// good for when
