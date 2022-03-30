3d42e50064f3a1fceb4f81d8e1a3c971d326d796
pcion <patrick.ciongoli@gmail.com>
2022-03-30 19:34:00 -0400

    trying to fix code

---
+++ controllers/home-routes.js
@@ -0,0 +1,19 @@
+// this file will contain all of the user-faceing routes
+// such as the homepage and login page
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.render('homepage', {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'Handlebars Docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user',
+    },
+  });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+
+router.use('/', homeRoutes);
 
 router.use('/api', apiRoutes);
-
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
-
 
 // file will collect the packaged API routes
 // and prefix them with the path /api
