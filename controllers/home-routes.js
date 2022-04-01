a1cbd195d8d31e8fbb292e7af3731738e266a5c4
pcion <patrick.ciongoli@gmail.com>
2022-04-01 14:38:51 -0400

    fixing single-post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,6 +48,56 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+        ),
+        'vote_count',
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData({ plain: true });
+
+      // pass data to template
+      res.render('single-post', { post });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // login route
 router.get('/login', (req, res) => {
   // check for a session and redirect to the homepage if one exists
